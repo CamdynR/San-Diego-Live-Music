048d91a8e2357f4bc282783a01662a4f05854153
Camdyn Rasque <rasque.camdyn@gmail.com>
2025-03-20 14:23:05 -0700

    Working on brick by brick

--- utilities/venues/brick-by-brick.ts
+++ utilities/venues/brick-by-brick.ts
@@ -1,30 +1,52 @@
 // brick-by-brick.ts
 
-import { Venue, Show, JSDOM } from '../Venue.ts';
+import jsdom from 'jsdom';
+const { JSDOM } = jsdom;
+// import { Venue, Show, JSDOM } from '../Venue.ts';
 
-export const brickByBrick: Venue = {
-  name: 'Brick by Brick',
-  address: '1130 Buenos Ave, San Diego, CA 92110',
-  url: 'https://www.brickbybrick.com/calendar/',
-  ages: '21+',
-  type: 'bar',
-  capacity: 400,
-  setting: 'indoors',
-  alcohol: 'full bar',
-  food: 'no food',
-  fetchSchedule
-};
+// export const brickByBrick: Venue = {
+//   name: 'Brick by Brick',
+//   address: '1130 Buenos Ave, San Diego, CA 92110',
+//   url: 'https://www.brickbybrick.com/calendar/',
+//   ages: '21+',
+//   type: 'bar',
+//   capacity: 400,
+//   setting: 'indoors',
+//   alcohol: 'full bar',
+//   food: 'no food',
+//   fetchSchedule
+// };
 
 async function fetchSchedule() {
-  const events: Show[] = [];
+  // const events: Show[] = [];
+  const events: any = [];
 
-  const response = await fetch(brickByBrick.url);
+  const response = await fetch('https://www.brickbybrick.com/calendar/');
   const data = await response.text();
   const dom = new JSDOM(data);
   const root = dom.window.document;
-  root.querySelectorAll('').forEach((element: HTMLElement) => {
-    // TODO
-  });
+  const scripts = [...root.querySelectorAll('script')];
+  const script = scripts.filter((element: HTMLElement) => {
+    return element.textContent?.includes('all_events');
+  })[0];
+
+  let allEvents: any = null;
+  if (script !== undefined) {
+    const match = script.textContent?.match(/var all_events\s*=\s*(\[.*?\]);/s);
+    if (match) {
+      try {
+        const parseJSObj = (jsLikeJson: string) =>
+          new Function(`return ${jsLikeJson}`)();
+        const obj = parseJSObj(match[1]);
+      } catch (e) {
+        console.error(`Failed to parse JSON: ${e}`);
+      }
+    }
+  }
+
+  console.log(allEvents);
 
   return events;
 }
+
+fetchSchedule();
